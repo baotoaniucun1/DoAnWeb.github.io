de4afbf8e1c38f587738873be317cd84db34906f
1660626 <1660626@student.hcmus.edu.vn>
2018-11-25 22:17:29 +0700

    First php project

---
+++ DoAnWebCuoiKy/DoAnWebCuoiKy/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <title>Chic Choe Shop</title>
+    
+    <?php
+    include 'modules/include.php';
+    ?>
+</head>
+
+<body>
+  <div>
+      <?php
+      include 'modules/header.php';
+      include 'modules/content.php';
+      include 'modules/footer.php';
+      ?>
+
+  </div>
+  
+</body>
+
+</html>
---
+++ DoAnWebCuoiKy/DoAnWebCuoiKy/modules/footer.php
@@ -0,0 +1,150 @@
+
+<footer class="footer" role="contentinfo">
+    <div class="container">
+        <section class="footer-info">
+            <article class="footer-info-col footer-info-col--small" data-section-type="footer-webPages">
+                <h5 class="footer-info-heading">Navigate</h5>
+                <ul class="footer-info-list">
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/journal/">Journal</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/our-story/">Our Story</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/contact-us/">
+                            Contact
+                            Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/shipping-returns/">
+                            Shipping
+                            &amp; Returns
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/sitemap.php">Sitemap</a>
+                    </li>
+                </ul>
+            </article>
+
+            <article class="footer-info-col footer-info-col--small" data-section-type="footer-categories">
+                <h5 class="footer-info-heading">Categories</h5>
+                <ul class="footer-info-list">
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/shop-all/garden/">Garden</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/all/">All</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/shop-all/kitchen/">Kitchen</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/shop-all/publications/">Publications</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/shop-all/utility/">Utility</a>
+                    </li>
+                </ul>
+            </article>
+
+            <article class="footer-info-col footer-info-col--small" data-section-type="footer-brands">
+                <h5 class="footer-info-heading">Popular Brands</h5>
+                <ul class="footer-info-list">
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/OFS.html">OFS</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Common-Good.html">
+                            Common
+                            Good
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Barr%252dCo..html">Barr-Co.</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Chemex.html">Chemex</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Kinfolk.html">Kinfolk</a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Able-Brewing.html">
+                            Able
+                            Brewing
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Fog-Linen-Work.html">
+                            Fog
+                            Linen Work
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Gather-Journal.html">
+                            Gather
+                            Journal
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Iris-Hantverk.html">
+                            Iris
+                            Hantverk
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://cornerstone-light-demo.mybigcommerce.com/brands/Openhouse-Magazine.html">
+                            Openhouse
+                            Magazine
+                        </a>
+                    </li>
+                    <li><a href="http://cornerstone-light-demo.mybigcommerce.com/brands/">View All</a></li>
+                </ul>
+            </article>
+
+            <article class="footer-info-col footer-info-col--small" data-section-type="storeInfo">
+                <h5 class="footer-info-heading">Info</h5>
+                <address>
+                    685 Market Street<br>
+                    San Francisco, CA 94105
+                </address>
+                <strong>Call us at (415) 555-5555</strong>
+            </article>
+
+            <article class="footer-info-col" data-section-type="newsletterSubscription">
+                <h5 class="footer-info-heading">Subscribe to our newsletter</h5>
+                <p>Get the latest updates on new products and upcoming sales</p>
+
+                <form class="form" action="http://cornerstone-light-demo.mybigcommerce.com/subscribe.php"
+                      method="post">
+                    <fieldset class="form-fieldset">
+                        <input type="hidden" name="action" value="subscribe">
+                        <input type="hidden" name="nl_first_name" value="bc">
+                        <input type="hidden" name="check" value="1">
+                        <div class="form-field">
+                            <label class="form-label is-srOnly" for="nl_email">Email Address</label>
+                            <div class="form-prefixPostfix wrap">
+                                <input class="form-input" id="nl_email" name="nl_email" type="email"
+                                       value="" placeholder="Your email address">
+                                <input class="button button--primary form-prefixPostfix-button--postfix"
+                                       type="submit" value="Subscribe">
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </article>
+        </section>
+        <div class="footer-copyright">
+            <p class="powered-by">
+                Powered by <a href="https://www.bigcommerce.com/?utm_source=merchant&amp;utm_medium=poweredbyBC"
+                              rel="nofollow">BigCommerce</a>
+            </p>
+        </div>
+        <div class="footer-copyright">
+            <p class="powered-by">© 2018 Cornerstone Demo </p>
+        </div>
+    </div>
+</footer>
