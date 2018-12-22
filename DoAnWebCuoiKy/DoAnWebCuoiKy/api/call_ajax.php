116300848720b39e53b06f2b9a62900221c9d09a
1660626 <1660626@student.hcmus.edu.vn>
2018-12-23 00:12:18 +0700

    search

---
+++ DoAnWebCuoiKy/DoAnWebCuoiKy/api/call_ajax.php
@@ -0,0 +1,29 @@
+﻿<?php
+require_once '../lib/db.php';
+$s1=$_REQUEST["n"];
+$select_query="select * from sanpham where tensanpham like '%".$s1."%'";
+$sql=load($select_query);
+$s="";
+while($row=$sql->fetch_assoc())
+{
+	$s=$s."
+<li>
+	<a class='link-p-colr' href='#'>
+
+
+                	<img  src='".$row['HinhURL']."'/>
+               <h3>".$row['TenSanPham']."</h3>
+             <span class='price'>".number_format($row['GiaSanPham'])."</span>
+
+
+	</a>
+</li>
+
+
+
+
+"	;
+}
+echo $s;
+?>
+
